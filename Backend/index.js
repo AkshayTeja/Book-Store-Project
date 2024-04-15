9a2b5c2b24ffb5136dd96a5120defb63bc68dd6e
Axay17 <akshayteja04@gmail.com>
2024-04-15 18:41:40 +0530

    commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,50 +1,42 @@
-import express from "express";
-import { PORT, mongoDBURL } from "./config.js";
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
-import { Book } from './models/bookModel.js'
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
-//Middleware for parsing request body
+// Middleware for parsing request body
 app.use(express.json());
 
-app.get('/',(request,response) => {
-    console.log(request);
-    return response.status(234).send('Welcome To MERN STACK');
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+
+app.get('/', (request, response) => {
+  console.log(request); 
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
-//Route for Saving a new book
-app.post('/books',async (request,response) => {
-    try {
-        if (
-            !request.body.title || !request.body.author || !request.body.publishYear
-        ){
-            return response.status(400).send({
-                message: 'Send all required fields: title author,publishYear',
-            });
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-        const book = await Book.create(newBook);
-        return response.status(201).send(book);
-    }
-    catch(error){
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
+app.use('/books', booksRoute);
 
 mongoose
-    .connect(mongoDBURL)
-    .then(() => {
-        console.log('App connected to database');
-        app.listen(PORT, () => {
-        console.log(`App is listening to port: ${PORT}`);
-        });
-    })
-.catch((error) => {
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
     console.log(error);
-});
+  });
